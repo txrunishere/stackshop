d7338847ef2ed157f088d1fbf2c40e88480974d3
txrunishere <tsoni9286@gmail.com>
2026-06-01 17:20:10 +0530

    init tanstack start project

---
+++ src/components/header.tsx
@@ -0,0 +1,7 @@
+export const Header = () => {
+  return (
+    <header className="w-full h-16 bg-gray-800 text-white flex items-center justify-between px-4 text-xl font-bold">
+      StackShop
+    </header>
+  )
+}
---
+++ src/routes/__root.tsx
@@ -0,0 +1,56 @@
+import { HeadContent, Scripts, createRootRoute } from '@tanstack/react-router'
+import { TanStackRouterDevtoolsPanel } from '@tanstack/react-router-devtools'
+import { TanStackDevtools } from '@tanstack/react-devtools'
+
+import appCss from '../styles.css?url'
+import { Header } from '#/components'
+
+export const Route = createRootRoute({
+  head: () => ({
+    meta: [
+      {
+        charSet: 'utf-8',
+      },
+      {
+        name: 'viewport',
+        content: 'width=device-width, initial-scale=1',
+      },
+      {
+        title: 'StackShop',
+      },
+    ],
+    links: [
+      {
+        rel: 'stylesheet',
+        href: appCss,
+      },
+    ],
+  }),
+  shellComponent: RootDocument,
+})
+
+function RootDocument({ children }: { children: React.ReactNode }) {
+  return (
+    <html lang="en" suppressHydrationWarning>
+      <head>
+        <HeadContent />
+      </head>
+      <body className="font-sans antialiased">
+        <Header />
+        {children}
+        <TanStackDevtools
+          config={{
+            position: 'bottom-right',
+          }}
+          plugins={[
+            {
+              name: 'Tanstack Router',
+              render: <TanStackRouterDevtoolsPanel />,
+            },
+          ]}
+        />
+        <Scripts />
+      </body>
+    </html>
+  )
+}
