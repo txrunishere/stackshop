c21f1daf5078386f15591792bced41a231b29626
txrunishere <tsoni9286@gmail.com>
2026-06-01 17:32:47 +0530

    add shadcn

--- src/routes/index.tsx
+++ src/routes/index.tsx
@@ -1,7 +1,12 @@
+import { Button } from '#/components/ui/button'
 import { createFileRoute } from '@tanstack/react-router'
 
 export const Route = createFileRoute('/')({ component: App })
 
 function App() {
-  return <div>APP</div>
+  return (
+    <div>
+      <Button>Click Me!</Button>
+    </div>
+  )
 }
